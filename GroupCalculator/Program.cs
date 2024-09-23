fe210c435e79fd417bca6dd6b0d5a96f49c9047c
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2024-09-23 13:58:19 +0200

    lagt till minus metod

--- GroupCalculator/Program.cs
+++ GroupCalculator/Program.cs
@@ -2,9 +2,33 @@
 {
     internal class Program
     {
+
+
+        static int Subtraction(int a, int b)
+        {
+
+            return a - b; 
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            /*int a = int.Parse(Console.ReadLine());
+
+             
+            string op = Console.ReadLine();
+
+            int b = int.Parse(Console.ReadLine());
+
+            double result = 0; 
+
+            switch (op)
+            {
+                case "-":
+                     result = Subtraction(a,b);
+                    Console.WriteLine(result);
+                    break;
+            }
+
+            */
         }
     }
 }
