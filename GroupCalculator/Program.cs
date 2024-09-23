19d127dbfb1cf9b6c62f16ba87f6daad98fb24bf
shobah24 <shokran.bahram14@gmail.se>
2024-09-23 14:16:02 +0200

    multiply

--- GroupCalculator/Program.cs
+++ GroupCalculator/Program.cs
@@ -4,17 +4,12 @@
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Skriv två siffror");
-            int num1 = int.Parse(Console.ReadLine());
-            int num2 = int.Parse(Console.ReadLine());
-
-            int multi = Multiply(num1, num2);
-            //Console.WriteLine($"{num1} x {num2} = {multi} ");
+           
         }
 
-        static int Multiply(int num1, int num2)
+        static int Multiply(int a, int b)
         {
-            return num1 * num2;
+            return a * b;
         }
     }
 }
