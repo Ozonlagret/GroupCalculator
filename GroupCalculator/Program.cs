41488fb8e40141b11848f3ff02cefbf097e170a6
TimiSUT24 <tim.petersenfalkenberg@outlook.com>
2024-09-23 14:07:22 +0200

    test

--- GroupCalculator/Program.cs
+++ GroupCalculator/Program.cs
@@ -11,24 +11,7 @@
         }
         static void Main(string[] args)
         {
-            /*int a = int.Parse(Console.ReadLine());
-
-             
-            string op = Console.ReadLine();
-
-            int b = int.Parse(Console.ReadLine());
-
-            double result = 0; 
-
-            switch (op)
-            {
-                case "-":
-                     result = Subtraction(a,b);
-                    Console.WriteLine(result);
-                    break;
-            }
-
-            */
+            
         }
     }
 }
